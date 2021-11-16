f81053d7466f579a34902ca945b067a9a8bb79bc
yand1960 <yand1960@gmail.com>
2021-11-16 16:30:55 +0300

    YA

--- JaAd04_DBs_Maven/src/main/java/my/dbs/JDBCDemo.java
+++ JaAd04_DBs_Maven/src/main/java/my/dbs/JDBCDemo.java
@@ -6,16 +6,20 @@ public class JDBCDemo {
 
     public static void main(String[] args) throws SQLException {
         String db_url = "jdbc:sqlserver://yand.dyndns.org;databaseName=AdventureWorks;";
-        Connection cn = DriverManager.getConnection(db_url,"northwind" ,"northwind");
+        double min_price = 3000.0;
 
-        String sql = "SELECT ProductID, Name, ProductNumber, ListPrice FROM Production.Product";
-        Statement stat = cn.createStatement();
-        ResultSet rs = stat.executeQuery(sql);
-        while(rs.next()) {
-            System.out.println(rs.getString("Name") + " - " + rs.getDouble("ListPrice"));
+        try(Connection cn = DriverManager.getConnection(db_url,"northwind" ,"northwind")) {
+            String sql = "SELECT ProductID, Name, ProductNumber, ListPrice " +
+                    "FROM Production.Product " +
+                    "WHERE ListPrice > ?";
+            PreparedStatement stat = cn.prepareStatement(sql);
+            stat.setDouble(1,min_price);
+            try(ResultSet rs = stat.executeQuery()) {
+                while (rs.next()) {
+                    System.out.println(rs.getString("Name") + " - " + rs.getDouble("ListPrice"));
+                }
+            }
         }
-        rs.close();
-        cn.close();
     }
 
 }
