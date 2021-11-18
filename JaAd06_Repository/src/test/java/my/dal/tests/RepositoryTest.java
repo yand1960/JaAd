eb76ef3d2e1c09b2af34833c69c659c1926d5de4
yand1960 <yand1960@gmail.com>
2021-11-18 10:20:57 +0300

    YA

--- JaAd06_Repository/src/test/java/my/dal/tests/RepositoryTest.java
+++ JaAd06_Repository/src/test/java/my/dal/tests/RepositoryTest.java
@@ -2,6 +2,7 @@ package my.dal.tests;
 
 import my.dal.Repository;
 import my.dal.RepositoryJDBC;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RepositoryTest {
@@ -10,6 +11,8 @@ public class RepositoryTest {
     public void getProductsTest() {
         Repository repository = new RepositoryJDBC();
         var result = repository.getProducts();
-        System.out.println(result.get(0).getName());
+        //System.out.println(result.get(0).getName());
+        Assertions.assertEquals(504, result.size());
+        Assertions.assertEquals("Adjustable Race", result.get(0).getName());
     }
 }
