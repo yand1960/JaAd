7f6326276c587d6c7a657ea2a5ede1d5c258e484
yand1960 <yand1960@gmail.com>
2021-11-19 13:40:08 +0300

    YA

--- JaAd11_Web/src/main/java/my/servlets/TimeServlet.java
+++ JaAd11_Web/src/main/java/my/servlets/TimeServlet.java
@@ -1,4 +1,20 @@
 package my.servlets;
 
-public class TimeServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name="time", urlPatterns = {"/time"})
+public class TimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Current time is "
+                                    + (new Date()).toString() + "</h1>");
+    }
 }
